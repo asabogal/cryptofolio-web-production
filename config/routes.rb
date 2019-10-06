e6b3246dddc0c5da303a83c3ee0dda377a7db3d7
asabogal <asabogal.git@gmail.com>
2019-10-06 11:09:40 -0400

    fix users routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
   
-  post '/signup', to: 'users#create'
+  resources :users, ony: [:create]
 end
