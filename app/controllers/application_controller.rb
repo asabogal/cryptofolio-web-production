3af83bb7f79955d2122e1068ec86bab5223a305a
asabogal <asabogal.git@gmail.com>
2019-10-06 11:07:58 -0400

    config application_controller auth and helper methods + fix cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+  helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!
 
   def login!
     session[:user_id] = @user.id
