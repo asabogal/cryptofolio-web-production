5568698dd5207b3bb0c26af94b6b54453f4d6af3
asabogal <asabogal.git@gmail.com>
2019-10-06 11:21:06 -0400

    init User controller with valid create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
+require 'pry'
+
 class UsersController < ApplicationController
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!
+      render json: {
+        status: :created,
+        user: @user
+      }
+    else 
+      render json: {status: 500}
+    end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
 end
