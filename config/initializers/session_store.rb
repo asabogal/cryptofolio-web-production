d30dfcdbb3579d5dd9a625d7fa111737a1cbf16b
asabogal <asabogal.git@gmail.com>
2019-10-08 23:00:57 -0400

    fix session store to set cookie on development

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,6 @@
-Rails.application.config.session_store :cookie_store, key: '_cryptofolio', domain: 'cryptofolio-api.herokuapp.com'
+if Rails.env === 'production' 
+  Rails.application.config.session_store :cookie_store, key: '_cryptofolio', domain: 'cryptofolio-api.herokuapp.com'
+else
+  Rails.application.config.session_store :cookie_store, key: '_cryptofolio' 
+end
+
