4878f08d673a509b27a6397c2bdc24bf9804dbde
asabogal <asabogal.git@gmail.com>
2019-10-08 21:08:51 -0400

    init sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,28 @@
 class SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by(email: session_params[:user][:email])
+  
+    if @user && @user.authenticate(session_params[:user][:password])
+      login!
+      render json: { 
+        status: :loggedIn
+        logged_in: true,
+        user: @user
+      }
+    else
+      render json: { 
+        status: 401,
+        errors: @user.errors.full_messages
+      }
+    end
+  end
+
+
+  private
+
+  def session_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
