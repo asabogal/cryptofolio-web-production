5ced0cb7a4317b33e45ab7ea592a4cc2cfd67803
asabogal <asabogal.git@gmail.com>
2019-10-09 22:08:15 -0400

    add logout functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {  };
   }
+
+  handleLogout = () => {
+    axios.delete('http://localhost:3001/logout', {withCredentials: true})
+    .then(response => this.props.handleLogout())
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div>
         <h1>HOME</h1>
         <h2>{this.props.loggedInStatus}</h2>
+        <button onClick={this.handleLogout}>LOGOUT</button>
       </div>
     );
   }
