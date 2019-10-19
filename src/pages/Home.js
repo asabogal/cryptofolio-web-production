58033d61d2c541fe9a918fb408e149ab66591ac0
asabogal <asabogal.git@gmail.com>
2019-10-18 22:47:55 -0400

    change to styled component + change loggedInStatus to boolean

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios'
-import './style.css'
+import styled from 'styled-components'
 
 class Home extends Component {
   constructor(props) {
@@ -16,13 +16,22 @@ class Home extends Component {
 
   render() {
     return (
-      <div className='page-container'>
+      <Container>
         <h1>HOME</h1>
-        <h2>{this.props.loggedInStatus}</h2>
-        <button onClick={this.handleLogout}>LOGOUT</button>
-      </div>
+        {
+          this.props.loggedInStatus ? <h2>Logged In</h2> : <h2>Not Logged In</h2>
+        }
+        {
+          this.props.loggedInStatus ? <button onClick={this.handleLogout}>LOGOUT</button> : null
+        }
+        
+      </Container>
     );
   }
 }
 
 export default Home;
+
+const Container = styled.div`
+  margin: 50px;
+`
