d220745468230c4d5cadb389366bb493134c025d
asabogal <asabogal.git@gmail.com>
2019-11-08 20:10:53 -0500

    init Header component

---
+++ src/components/header_footer/Header.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      isLoggedIn: false
+     };
+  }
+
+  render() {
+    return (
+      <Container>
+        <div>
+          LOGO
+        </div>
+
+        <ul>
+          <Link to='/dashboard'><li>Dashboard</li></Link>
+          <Link to='/settings'><li>Settings</li></Link>
+          <Link to='/logout'><li>Log Out</li></Link>
+        </ul>
+      </Container>
+    );
+  }
+}
+
+export default Header;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* border: 1px solid black; */
+  height: 100px;
+  width: 100%;
+  background: none;
+  ul {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding-right: 50px;
+  }
+  a {
+    text-decoration: none;
+    color: white;
+  }
+  li {
+    list-style: none;
+    text-decoration: none;
+    padding: 20px;
+  }
+`;
