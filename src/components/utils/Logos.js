1649b949b8b081ed700a6f2eba1da92c07253d7b
asabogal <asabogal.git@gmail.com>
2019-11-09 22:50:49 -0500

    make Logo dynamic and reusable

--- src/components/utils/Logos.js
+++ src/components/utils/Logos.js
@@ -3,19 +3,24 @@ import styled from 'styled-components'
 import {Link} from 'react-router-dom'
 import logo from '../../images/crypto-vault.png'
 
-export const NavLogo = () => {
-  return (
-    <Link to='/'>
-      <LogoContainer/>
-    </Link>
-  )
- 
+export const Logo = (props) => {
+  if (props.link) {
+    return (
+      <Link to='/'>
+        <LogoContainer width={props.width} height={props.height}/>
+      </Link>
+      )
+  } else {
+    return <LogoContainer width={props.width} height={props.height}/>
+  }
+
 }
+
 
 const LogoContainer = styled.div`
   background: url(${logo}) no-repeat;
   background-size: cover !important;
-  width: 50px;
-  height: 50px;
+  width: ${props => props.height};
+  height: ${props => props.width};
   opacity: .6;
 `;
