04cfadb15723191a91d500f41d48848ebb58abed
asabogal <asabogal.git@gmail.com>
2019-11-12 23:02:58 -0500

    init settings components; Form

---
+++ src/components/settings/Form.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import styled from 'styled-components'
+import {Button} from '../utils/Buttons'
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      coinSymbol: '',
+      coins: []
+     };
+  }
+
+  handleChange = (event) => {
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+  };
+
+  render() {
+    const {coinSymbol} = this.state
+    return (
+      <FormContainer>
+        <h3>Coin Lookup:</h3>
+        <form>
+          <input
+            placeholder='enter coin symbol'
+            type='text'
+            name='coinSymbol'
+            value={coinSymbol}
+            onChange={this.handleChange}
+          />
+           <Button 
+          type="submit"
+          width='100%'
+          height='35px'
+          font='12px'
+          >
+            Look Up
+          </Button>
+        </form>
+      </FormContainer>
+    );
+  }
+}
+
+export default Form;
+
+//styled
+const FormContainer = styled.div`
+  display: grid;
+
+  form {
+    width: 300px;
+    display: grid;
+    grid-template-columns: 250px 100px;
+    grid-gap: 10px
+  }
+  input {
+    border: 1px thin black;
+    height: 20px;
+    font-size: 1rem;
+    padding: 5px;
+  }
+  `;
