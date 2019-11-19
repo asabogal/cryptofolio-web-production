46874db04ea9fddf2fcd32bb7f88021043ae8a05
asabogal <asabogal.git@gmail.com>
2019-11-19 01:04:35 -0500

    init coin service with find_coin method

--- app/services/coin_service.rb
+++ app/services/coin_service.rb
@@ -1,2 +1,13 @@
 class CoinService
+
+  def self.find_coin(symbol)
+    url = "https://min-api.cryptocompare.com/data/coin/generalinfo?fsyms=#{symbol}&tsym=USD"
+    resp = Faraday.get url do |req|
+    end
+
+    @body = JSON.parse(resp.body)
+
+  end
+   @body
+
 end
