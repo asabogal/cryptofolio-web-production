8555d758c8bc7f1fc9c8df3aedec93a47cbe5313
asabogal <asabogal.git@gmail.com>
2019-11-19 02:23:42 -0500

    add getCoin, addCoin, renderUserCoins functionality

--- src/components/settings/Content.js
+++ src/components/settings/Content.js
@@ -1,19 +1,85 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import Form from './Form'
+import CoinCard from '../coins/CoinCard'
+import {ErrorsContainer} from '../registrations/styled'
 
 class Content extends Component {
   constructor(props) {
     super(props);
     this.state = { 
       userCoins: [],
-      allCoins: []
+      allCoins: [],
+      errors: ''
      };
   }
 
+  getCoin = (symbol) => {
+    let url = `http://localhost:3001/coins/${symbol}`
+    const config = {
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json'
+      }
+    }
+    axios.get(url, config)
+    .then((response) => {
+      response.data.errors ? this.setState({
+        errors: response.data.errors
+      }) :
+      this.addCoin(response.data.coin.CoinInfo)
+    })
+    .then(() => console.log(this.state.userCoins))
+  }
+
+  addCoin = (data) => {
+    let coin = {
+      symbol: data.Name,
+      name: data.FullName,
+      image: `https://www.cryptocompare.com${data.ImageUrl}`
+    }
+    this.setState({
+      userCoins: this.state.userCoins.concat(coin)
+    })
+  }
+
+  renderUserCoins = () => {
+    const coins = this.state.userCoins
+    return coins.map(coin => {
+      return (
+        <CoinCard 
+        key={coin.symbol}
+        symbol={coin.symbol}
+        name={coin.name}
+        image={coin.image}
+        />
+      )
+    })
+  }
+
+  handleErrors = () => {
+    return (
+      <ErrorsContainer>
+        <ul>
+        {this.state.errors.map(error => {
+        return <li style={{padding: '2px'}} key={error}>{error}</li>
+          })
+        }
+        </ul>
+      </ErrorsContainer>
+    )
+  }
+    
   render() {
     return (
       <div>
-        <Form/>
+        {this.renderUserCoins()}
+        <Form updateState={this.updateState} getCoin={this.getCoin}/>
+        <div>
+          {
+            this.state.errors ? this.handleErrors() : null
+          }
+        </div>
       </div>
     );
   }
