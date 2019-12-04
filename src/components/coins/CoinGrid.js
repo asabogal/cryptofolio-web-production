e1d91116315c2043f0310df3ff38b622ce8499f9
asabogal <asabogal.git@gmail.com>
2019-12-03 20:57:54 -0500

    better grid layout

--- src/components/coins/CoinGrid.js
+++ src/components/coins/CoinGrid.js
@@ -2,10 +2,12 @@ import styled from 'styled-components'
 
 const CoinGrid = styled.div`
   display: grid;
-  grid-template-columns: ${(props => props.columns || '1fr 1fr 1fr 1fr 1fr')};
+  grid-template-columns: ${(props => props.columns || 'repeat(auto-fit, minmax(210px, 1fr))')};
   grid-template-rows: auto;
+  justify-items: center;
+  align-items: center;
   grid-gap: 15px;
-  margin: 30px;
+  margin: 30px 10px 30px 10px;
 `;
 
 export default CoinGrid;
