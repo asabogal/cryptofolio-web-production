eec5e390e318e583c90ff355e55bedd512a09546
asabogal <asabogal.git@gmail.com>
2019-12-06 23:17:36 -0500

    clean

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Content from '../components/home/Content'
-import Header from '../components/header_footer/Header'
 
 class Home extends Component {
   constructor(props) {
