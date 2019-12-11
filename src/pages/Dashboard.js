f6df89b2c9af40b7fbcc64618a646b93979ef81c
asabogal <asabogal.git@gmail.com>
2019-12-10 22:06:33 -0500

    init full Dashboard; refactor to components

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import UserCoin from '../components/coins/UserCoin'
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = {  };
+    this.state = { 
+      userCoins: []
+     };
   }
+
+  componentDidMount() {
+    this.getUserCoins(this.props.user)
+  }
+
+
+  getUserCoins = (user) => {
+    const userId = user.id
+    const url = `http://localhost:3001/users/${userId}/user_coins`
+    const config = {
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json'
+      }
+  }
+
+    axios.get(url, config)
+    .then(response => {
+      this.setState({
+        userCoins: response.data.coins
+      })
+    })
+  }
+
+  renderCoins = (coins) => {
+    return coins.map(coin => {
+      return (
+        <UserCoin
+          key={coin.symbol}
+          symbol={coin.symbol}
+          price={coin.price}
+          changeDay={coin.changeDay}
+          changePct={coin.changePct}
+          mrktCap={coin.mrktCap}
+        />
+      )
+    })
+  }
+
   render() {
+  
     return (
       <div>
         <h1>DASHBOARD</h1>
         {
-          this.props.loggedInStatus ? <h2>Logged In</h2> : <h2>Not Logged In</h2>
+          this.renderCoins(this.state.userCoins)
         }
-       
       </div>
     );
   }
