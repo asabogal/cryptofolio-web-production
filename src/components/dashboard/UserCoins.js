51bc8b5b8f435a5ee8ed4d5d407eb80b3d6a30dc
asabogal <asabogal.git@gmail.com>
2019-12-12 23:48:47 -0500

    init dashboard UserCoins component

---
+++ src/components/dashboard/UserCoins.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import UserCoin from '../coins/UserCoin'
+import CoinGrid from '../coins/CoinGrid'
+
+class UserCoins extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      userCoins: ''
+     };
+  }
+
+  componentDidMount() {
+    console.log('user coins did mount', this.props)
+    this.getUserCoins(this.props.user)
+  } 
+
+  getUserCoins = (user) => {
+    const userId = user.id
+    const url = `http://localhost:3001/users/${userId}/user_coins`
+    const config = {
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json'
+        }
+    }
+
+    axios.get(url, config).then(response => {
+      this.setState({
+        userCoins: response.data.coins
+      })
+    })
+  }
+
+
+  renderCoins = (coins) => {
+    return coins.map(coin => {
+      return (
+        <UserCoin
+          key={coin.symbol}
+          symbol={coin.symbol}
+          price={coin.price}
+          changeDay={coin.changeDay}
+          changePct={coin.changePct}
+          mrktCap={coin.mrktCap}
+        />
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div>
+      {
+        this.state.userCoins ? (
+          <CoinGrid>
+          {this.renderCoins(this.state.userCoins)}
+        </CoinGrid>
+        ) :
+        null
+      }
+      </div>
+    );
+  }
+}
+
+export default UserCoins;
