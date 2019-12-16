d759451448b01a928df23d17264235b4b01cebbc
asabogal <asabogal.git@gmail.com>
2019-12-15 22:08:00 -0500

    init InfoLogo

---
+++ src/components/dashboard/InfoLogo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components'
+import {Logo} from '../utils/Logos'
+
+const InfoLogo = (props) => {
+  let sourceUrl = `https://www.cryptocompare.com/${props.selectedCoin.imageUrl}`
+  return (
+    <Container>
+      <h2>{props.selectedCoin.symbol}</h2>
+      <Logo logo={sourceUrl} height='250px' width='250px' opacity='.8'/>
+    </Container>
+  );
+};
+
+export default InfoLogo;
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 2fr;
+  justify-items: center;
+  align-items: center;
+  background-color: #212542;
+  padding-bottom: 50px;
+`;
