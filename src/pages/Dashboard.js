1332e5071db5169a11644844467869aae346f4be
asabogal <asabogal.git@gmail.com>
2019-12-18 01:31:11 -0500

    add changeChartRange + expand getHistoricalData

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -11,6 +11,7 @@ class Dashboard extends Component {
       user: '',
       selectedCoin: '',
       active: false,
+      chartRange: 'months',
       historicalData: '',
       errors: ''
      };
@@ -21,24 +22,29 @@ class Dashboard extends Component {
       user: this.props.user
     })
   }
-
+  
   setSelectedCoin = (coin) => {
     this.setState({
       selectedCoin: coin,
       active: true,
       historicalData: null
     })
-    this.getHistoricalData(coin.symbol)
+    this.getHistoricalData(coin.symbol, this.state.chartRange)
   }
 
-  getHistoricalData = (symbol, period="DIGITAL_CURRENCY_MONTHLY") => {
+  getHistoricalData = (symbol, period) => {
+    const ranges = {
+      days: "DIGITAL_CURRENCY_DAILY",
+      weeks: "DIGITAL_CURRENCY_WEEKLY",
+      months: "DIGITAL_CURRENCY_MONTHLY"
+    }
     const url = "http://localhost:3001/history"
     const params = {
       symbol: symbol,
-      period: period
+      period: ranges[period]
     }
+    
     axios.get(url, {params})
-
     .then(response => {
       if (response.data.errors) {
         this.setState({
@@ -52,6 +58,13 @@ class Dashboard extends Component {
     })
   }
 
+  changeChartRange = (value) => {
+    this.setState({
+      chartRange: value,
+      historicalData: null
+    }, this.getHistoricalData(this.state.selectedCoin.symbol, value))
+  }
+
   chartData = () => {
     return this.state.errors ? this.state.errors : this.state.historicalData
   }
@@ -63,7 +76,7 @@ class Dashboard extends Component {
           this.state.user ? <UserCoins user={this.state.user} setSelectedCoin={this.setSelectedCoin} isActive={this.state.active}/> : null
         }
         {
-          this.state.active ? <InfoContainer selectedCoin={this.state.selectedCoin} chartData={this.chartData()}/> : null
+          this.state.active ? <InfoContainer selectedCoin={this.state.selectedCoin} chartData={this.chartData()} changeChartRange={this.changeChartRange}/> : null
         }
       </PageWrapper>
     );
