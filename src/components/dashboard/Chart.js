a5c15a1ef65b2defd82b38141f2437b0fa6b77cf
asabogal <asabogal.git@gmail.com>
2019-12-18 01:28:48 -0500

    add ChartRange and pass change handler

--- src/components/dashboard/Chart.js
+++ src/components/dashboard/Chart.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
 import ReactHighCharts from 'react-highcharts'
 import highchartsConfig from './HighChartsConfig'
 import highchartsTheme from './HighChartsTheme'
+import {ChartRange} from './ChartRange'
 
 ReactHighCharts.Highcharts.setOptions(highchartsTheme);
 
@@ -15,25 +17,40 @@ class Chart extends Component {
     if (!this.props.chartData) {
       return <div>Loading...</div>
     } else if (typeof this.props.chartData === 'string') {
-      return <div style={errorStyle}>{this.props.chartData}</div>
+      return <Errors>{this.props.chartData}</Errors>
     } else {
-      return <ReactHighCharts config={highchartsConfig(this.props.chartData)}/>
+      return (
+        <div>
+          <ChartRange defaultValue="months" onChange={e => this.props.changeChartRange(e.target.value)}>
+            <option value="days">Days</option>
+            <option value="weeks">Weeks</option>
+            <option value="months">Months</option>
+          </ChartRange>
+          <ReactHighCharts config={highchartsConfig(this.props.chartData)}/>
+        </div>
+      )
     }
   }
   render() {
     return (
-      <div>
+      <ChartContainer>
         {
           this.renderChart()
         }
-      </div>
+      </ChartContainer>
     );
   }
 }
 
 export default Chart;
 
-const errorStyle = {
-  color: 'red',
-  textAlign: 'center',
-}
+const ChartContainer = styled.div`
+  background-color: #212542;
+`;
+
+const Errors = styled.div`
+  color: red;
+  text-align: center;
+  margin-top: 50px;
+`;
+
