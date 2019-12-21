f92d1bc0b449f087cd6aea171c766582aeb55894
asabogal <asabogal.git@gmail.com>
2019-12-20 20:40:23 -0500

    add fallback route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   helper_method :login!, :logged_in?, :current_user, :authorized_user?, :logout!
 
+  def fallback_index_html
+    render :file => 'public/index.html'
+  end
+
   def login!
     session[:user_id] = @user.id
   end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,9 @@ Rails.application.routes.draw do
   resources :coins, only: [:index, :show, :create, :destroy ], param: :slug
   get '/history', to: 'coins#get_historical_data'
   get '/democoins', to: 'coins#get_demo_coins'
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
+  
 end
