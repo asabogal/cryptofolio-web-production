6e1f09397551860f8315ff6fabc9ebd657e5f67d
asabogal <asabogal.git@gmail.com>
2019-12-20 22:15:53 -0500

    change session_storage config for heroku

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_cryptofolio', domain: 'cryptofolio-api.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: '_cryptofolio', domain: 'cryptofolio-web-app.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store, key: '_cryptofolio' 
 end
